a1d14d74fa1acafc658e5f2bf20c77ed63edf52a
Gurylev Sergei Vitalevich <s.gurylev@haulmont.com>
2022-06-15 13:03:48 +0400

    Demo App

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,52 @@
+import json
+
 from flask import Flask, request, jsonify
 from transformers import pipeline
 from sentence_transformers import SentenceTransformer, util
 from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+import numpy as np
 
 debug_mode = False
 
 app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+
+class CacheModels:
+    def __init__(self, size=2):
+        self.max_size = size
+        self.models = dict()
+        self.list_models = list()
+
+    def contains(self, model_name, task):
+        return model_name + '_' + task in self.models.keys()
+
+    def put(self, model_name, task, model):
+        if len(self.models) == self.max_size:
+            del self.models[self.list_models[0]]
+            self.list_models = self.list_models[1:]
+        self.models[model_name+'_'+task] = model
+        self.list_models.append(model_name+'_'+task)
+
+    def get(self, model_name, task):
+        if self.contains(model_name, task):
+            return self.models[model_name+'_'+task]
+        else:
+            return None
+
+
+class NpEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        if isinstance(obj, np.floating):
+            # 👇️ alternatively use str()
+            return float(obj)
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return json.JSONEncoder.default(self, obj)
+
+
+cache = CacheModels()
 
 
 @app.route('/classification', methods=['POST'])
@@ -14,8 +55,12 @@ def get_answer():
     model, text = \
         request_data['model'],\
         request_data['text']
-    ppl = pipeline("text-classification", model)
-    return str(ppl(text))
+    if cache.contains("text-classification", model):
+        ppl = cache.get("text-classification", model)
+    else:
+        ppl = pipeline("text-classification", model)
+        cache.put("text-classification", model, ppl)
+    return json.dumps(ppl(text))
 
 
 @app.route('/ner', methods=['POST'])
@@ -24,8 +69,12 @@ def get_ner():
     model, text = \
         request_data['model'],\
         request_data['text']
-    ner_pipeline = pipeline("ner", model, aggregation_strategy="first")
-    return str(ner_pipeline(text))
+    if cache.contains("ner", model):
+        ner_pipeline = cache.get("ner", model)
+    else:
+        ner_pipeline = pipeline("ner", model, aggregation_strategy="first")
+        cache.put("ner", model, ner_pipeline)
+    return json.dumps(ner_pipeline(text), cls=NpEncoder)
 
 
 @app.route('/sentence_sim', methods=['POST'])
@@ -33,15 +82,17 @@ def get_sentence_sim():
     request_data = request.json
     model, sentences = \
         request_data['model'], \
-        request_data['text']
+        request_data['sentences']
+    if cache.contains("sentence_sim", model):
+        model = cache.get("sentence_sim", model)
+    else:
+        model = SentenceTransformer(model)
+        cache.put("sentence_sim", model, model)
 
-    model = SentenceTransformer(model)
-    embeddings = []
-    for sentence in sentences:
-        embeddings.append(model.encode(sentence, convert_to_tensor=True))
     cos_sim = dict()
-    for emb in embeddings[1:]:
-        cos_sim[emb] = util.pytorch_cos_sim(embeddings[0], emb)
+    for sentence in sentences[1:]:
+        cos_sim[sentence] = util.pytorch_cos_sim(model.encode(sentences[0], convert_to_tensor=True),
+                                                 model.encode(sentence, convert_to_tensor=True)).item()
     return cos_sim
 
 
@@ -52,7 +103,7 @@ def get_qa():
         request_data['model'],\
         request_data['question'], request_data['context']
     qa = pipeline("question-answering", model)
-    return str(qa(question, context))
+    return json.dumps(qa(question, context), cls=NpEncoder)
 
 
 @app.route('/generation', methods=['POST'])
@@ -62,7 +113,7 @@ def generate():
         request_data['model'], \
         request_data['text']
     generator = pipeline('text-generation', model=model)
-    return generator(sentences)
+    return json.dumps(generator(sentences))
 
 
 @app.route('/seq2seq', methods=['POST'])
@@ -72,7 +123,7 @@ def get_seq2seq():
         request_data['model'], \
         request_data['text']
     seq2seq = pipeline('text2text-generation', model=model)
-    return seq2seq(sentences)
+    return json.dumps(seq2seq(sentences))
 
 
 @app.route('/translation', methods=['POST'])
@@ -82,26 +133,31 @@ def get_translation():
         request_data['model'], request_data['from'], request_data['to'],\
         request_data['text']
     ppl = pipeline("translation_" + from_lang + "_to_" + to_lang, model)
-    return str(ppl(text))
+    return json.dumps(ppl(text))
 
 
 @app.route('/summarization', methods=['POST'])
 def get_summarization():
     request_data = request.json
-    model, task, text = \
-        request_data['model'], request_data['task'],\
+    model, text = \
+        request_data['model'],\
         request_data['text']
-    ppl = pipeline(task, model)
-    return str(ppl(text))
+    ppl = pipeline("summarization", model)
+    return json.dumps(ppl(text))
 
 
 @app.route('/closed-book', methods=['POST'])
 def get_qa_closed_book():
-    t5_qa_model = AutoModelForSeq2SeqLM.from_pretrained("google/t5-small-ssm-nq")
-    t5_tok = AutoTokenizer.from_pretrained("google/t5-small-ssm-nq")
-    input_ids = t5_tok("When was Franklin D. Roosevelt born?", return_tensors="pt").input_ids
-    gen_output = t5_qa_model.generate(input_ids)[0]
-    print(t5_tok.decode(gen_output, skip_special_tokens=True))
+    request_data = request.json
+    model, text = \
+        request_data['model'], \
+        request_data['text']
+
+    qa_model = AutoModelForSeq2SeqLM.from_pretrained(model)
+    tok = AutoTokenizer.from_pretrained(model)
+    input_ids = tok(text, return_tensors="pt").input_ids
+    gen_output = qa_model.generate(input_ids)[0]
+    return json.dumps(tok.decode(gen_output, skip_special_tokens=True))
 
 
 app.debug = False
--- frontend/script.js
+++ frontend/script.js
@@ -1,11 +1,94 @@
-document.getElementsByClassName("btn-primary").addEventListener('click', function (e) {
-    e.targets.disabled = true
+let buttons = document.querySelectorAll("button.btn-primary")
+
+Array.from(buttons).forEach(button => {
+    button.addEventListener('click', function (e) {
+    e.target.disabled = true
     let request = new XMLHttpRequest();   // new HttpRequest instance
-    request.open("POST", 'api/answer');
+    let url = ''
+        let values = {}
+    switch (document.querySelector(".active").id){
+        case "nav-classification":
+            url = "api/classification"
+            values = {
+               "model": document.querySelector(".active input.model-input").value,
+               "text": document.querySelector(".active input.text-input").value
+            }
+            break;
+        case "nav-ner":
+            url = "api/ner"
+            values = {
+               "model": document.querySelector(".active input.model-input").value,
+               "text": document.querySelector(".active input.text-input").value
+            }
+            break;
+        case "nav-similarity":
+            url = "api/sentence_sim"
+            let sentences = [];
+            let inps = document.querySelectorAll(".active input.text-input")
+            Array.from(inps).forEach(inp => {sentences.push(inp.value);});
+
+            values = {
+               "model": document.querySelector(".active input.model-input").value,
+               "sentences": sentences
+            }
+            break;
+        case "nav-qa":
+            url = "api/qa"
+            values = {
+               "model": document.querySelector(".active input.model-input").value,
+               "question": document.querySelector(".active input.question-input").value,
+                "context": document.querySelector(".active input.context-input").value
+            }
+            break;
+        case "nav-generation":
+            url = "api/generation"
+            values = {
+               "model": document.querySelector(".active input.model-input").value,
+               "text": document.querySelector(".active input.text-input").value
+            }
+            break;
+        case "nav-seq2seq":
+            url = "api/seq2seq"
+            values = {
+               "model": document.querySelector(".active input.model-input").value,
+               "text": document.querySelector(".active input.text-input").value
+            }
+            break;
+        case "nav-translation":
+            url = "api/translation"
+            let from = document.querySelector("#from-lang").value
+            let to = document.querySelector("#to-lang").value
+            values = {
+               "model": document.querySelector(".active input.model-input").value,
+               "text": document.querySelector(".active input.text-input").value,
+                "from": from,
+                "to": to
+            }
+            break;
+        case "nav-summarization":
+            url = "api/summarization"
+            values = {
+               "model": document.querySelector(".active input.model-input").value,
+               "text": document.querySelector(".active input.text-input").value
+            }
+            break;
+        case "nav-qa-closed-book":
+            url = "api/closed-book"
+            values = {
+               "model": document.querySelector(".active input.model-input").value,
+               "text": document.querySelector(".active input.text-input").value
+            }
+            break;
+
+    }
+
+    request.open("POST", url);
     request.setRequestHeader("Content-Type", "application/json");
     request.onload = function () {
         let status = request.status;
         if (status === 200) {
+            document.querySelector(".active .output").textContent = JSON.stringify(JSON.parse(request.response),
+                null, 2)
             // let answerDiv = document.getElementById("answer-div");
             // let buttonP = document.getElementById('button-p')
             // answerDiv.innerHTML = '';
@@ -29,13 +112,11 @@ document.getElementsByClassName("btn-primary").addEventListener('click', functio
         } else {
             console.log(status);
         }
-        e.targets.disabled = false
+        e.target.disabled = false
     };
-    request.send(JSON.stringify({
-        "industries": document.getElementById('industries-text').value,
-        "targets": document.getElementById('target-text').value,
-        "currents": document.getElementById('current-text').value,
-        "faults": document.getElementById('faults-text').value,
-        "model": document.getElementById('ml-model').value
-    }));
+
+
+    request.send(JSON.stringify(values));
 })
+
+});
